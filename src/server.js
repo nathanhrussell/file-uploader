fc37d9a765fdc3bcb038419a352ba29bddca63b0
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-25 16:23:19 +0200

    Add log out button to home page and upload pages; remove server running message

--- src/server.js
+++ src/server.js
@@ -23,7 +23,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
-});
