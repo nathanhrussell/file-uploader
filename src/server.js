a0f00c47543a61107b841aef014ff76d9984255c
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-25 16:27:09 +0200

    Pass Prisma client to the session store in server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,30 @@ const authRoutes = require("./auth/routes");
 const app = require("./app");
 require("./auth/passport");
 
+const { PrismaClient } = require("@prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+
+const prisma = new PrismaClient();
+
+const session = require("express-session");
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  },
+  store: new PrismaSessionStore(
+    prisma, //
+    {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined
+    }
+  )
+}));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +40,6 @@ app.use(authRoutes);
 
 const dotenv = require("dotenv");
 dotenv.config();
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
