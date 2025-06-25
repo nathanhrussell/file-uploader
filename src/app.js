6d2e5517e012361972b7359176e0630ead595a60
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-25 16:54:19 +0200

    Move route registration to server.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./auth/routes");
-const fileRoutes = require("../src/files/routes");
-const folderRoutes = require("../src/folders/routes");
 const path = require("path");
-require("./auth/passport");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(authRoutes);
-app.use(fileRoutes);
-app.use(folderRoutes);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const expressSession = require("express-session");
 const passport = require("passport");
+require("./auth/passport");
+const authRoutes = require("./auth/routes");
+const fileRoutes = require("./files/routes");
+const folderRoutes = require("./folders/routes");
+
 const { PrismaClient } = require("@prisma/client");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
-const app = require("./app");
-require("dotenv").config();
-require("./auth/passport");
 
+const app = require("./app");
 const prisma = new PrismaClient();
 
 app.use(expressSession({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  },
+  cookie: { maxAge: 1000 * 60 * 60 * 24 },
   store: new PrismaSessionStore(prisma, {
     checkPeriod: 2 * 60 * 1000,
     dbRecordIdIsSessionId: true
@@ -24,6 +26,10 @@ app.use(expressSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(authRoutes);
+app.use(fileRoutes);
+app.use(folderRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
