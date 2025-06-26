2b1ea13b90824aa2ac7e67101feaa4d4161f8019
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-26 10:21:38 +0200

    Remove duplicate code in home page and refactor logout.js

--- public/logout.js
+++ public/logout.js
@@ -1,15 +1,15 @@
 async function logout() {
-    const res = await fetch("/logout", {
-        method: "GET",
-        credentials: "include"
-    });
+  const res = await fetch("/logout", {
+    method: "GET",
+    credentials: "include"
+  });
 
-    if (res.ok) {
-        showMessage("Logged out");
-        setTimeout(() => {
-            window.location.href = "/index.html";
-        }, 1000);
-    } else {
-        showMessage("Logout failed");
-    }
+  if (res.ok) {
+    showMessage("Logged out successfully");
+    setTimeout(() => {
+      window.location.href = "/index.html";
+    }, 1000);
+  } else {
+    showMessage("Failed to log out", true);
+  }
 }
